a606c7eb2d77fe387ceab294ee3de46e47a64322
ElBoyTM <nszalenski@gmail.com>
2024-10-11 15:59:56 -0400

    created first draft of Tow action

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -388,7 +388,13 @@ class Cli {
         }
         // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
         else if (answers.action === 'Tow') {
-          
+          if (Truck) {
+            this.findVehicleToTow;
+            return;
+          } else {
+            console.log(`Your selected vehicle is incapable of towing`);
+            return;
+          };
         }
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
         else if (answers.action === 'Select or create another vehicle') {
